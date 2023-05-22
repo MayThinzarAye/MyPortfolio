a11bee5208817ff89db97882fda442d94e1ba154
MayThinzarAye <maythinzaraye7777@gmail.com>
2023-05-22 17:24:51 +0630

    end

--- js/script.js
+++ js/script.js
@@ -42,3 +42,27 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
+
+// button more detail
+const btnMoreDetail = document.querySelector(".btn--more-detail");
+btnMoreDetail.addEventListener("click", function (e) {
+  e.preventDefault();
+  const id = btnMoreDetail.getAttribute("href");
+  console.log(id);
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
+// hero textwriting animation
+const text = document.querySelector(".second-text");
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = "Web Developer";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Web Designer";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Freelancer";
+  }, 8000);
+};
+setInterval(textLoad, 12000);
